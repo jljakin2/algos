ea091da924e9009e666c4fc3947634bf7f4cea63
jljakin2 <jeff.jakinovich@gmail.com>
2021-12-06 13:24:37 -0500

    bfs for trees

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -58,4 +58,22 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  // Breadth-First Search
+  BFS() {
+    let data = [];
+    let queue = [];
+    let node = this.root;
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value); // using the value makes it easier to see what the data actually is. otherwise, it will be nodes
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return data;
+  }
 }
