c86aff5ebefa1d98e443cca4d9eb1565b7228abb
jljakin2 <jeff.jakinovich@gmail.com>
2021-12-10 15:04:38 -0500

    addEdge method to Graph class

--- graphs.js
+++ graphs.js
@@ -10,4 +10,12 @@ class Graph {
       this.adjacencyList[vertex] = []; // create the key with the value of the vertex in the adjacency list and set it equal to an empty array which is where we are going to keep its connections
     }
   }
+
+  // add new edge to our graph
+  addEdge(v1, v2) {
+    // takes in two vertexes
+    // NOTE: this is a very basic implementation. it doesn't have error checking for vertex types and already existing edges. REMEMBER to add those in when interviewing
+    this.adjacencyList[v1].push(v2); // find the first vertex in the adjacency list and push the second vertex to its array
+    this.adjacencyList[v2].push(v1); // find the second vertex in the adjacency list and push the first vertex to its array
+  }
 }
