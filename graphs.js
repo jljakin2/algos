35f5a56e0448c22f1a52908cdd32b59fcadb3f8a
jljakin2 <jeff.jakinovich@gmail.com>
2021-12-10 15:14:12 -0500

    removeEdge method for Graph class

--- graphs.js
+++ graphs.js
@@ -18,4 +18,18 @@ class Graph {
     this.adjacencyList[v1].push(v2); // find the first vertex in the adjacency list and push the second vertex to its array
     this.adjacencyList[v2].push(v1); // find the second vertex in the adjacency list and push the first vertex to its array
   }
+
+  // remove an edge from our graph
+  removeEdge(vertex1, vertex2) {
+    // takes in two vertexes
+    // NOTE: this is a very basic implementation. it doesn't have error checking for vertex types and already existing edges. REMEMBER to add those in when interviewing
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      // find the first vertex and set its adjacency list equal to a filtered version of the current list that doesn't include the second vertex
+      v => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      // find the second vertex and set its adjacency list equal to a filtered version of the current list that doesn't include the first vertex
+      v => v !== vertex1
+    );
+  }
 }
